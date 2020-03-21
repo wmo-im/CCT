2588ae92ab8ed5f1e11d0acccdb2d614839f9d91
Enrico Fucile <enrico.fucile@ecmwf.int>
2020-03-21 10:12:44 +0100

    version 20190515

--- scripts/convert_common.py
+++ scripts/convert_common.py
@@ -9,16 +9,14 @@ import os
 
 dirname = sys.argv[1]
 for infilename in os.listdir(dirname):
-    if infilename.endswith(".txt"):
-        outfilename=dirname+'/'+infilename[7:10]+'.csv'
+    if infilename.endswith(".txt") and infilename.startswith("Common"):
+        outfilename=infilename[7:10]+'.csv'
         with open(dirname + '/' + infilename, encoding = 'utf-8') as csvFile:
             csvReader = csv.reader(csvFile, delimiter = ',', quotechar='"')
             outfile = open(outfilename, 'w', encoding='utf-8', newline='')
-            outfilewriter = csv.writer(outfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            outfilewriter = csv.writer(outfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL,lineterminator='\n')
 
             for row in csvReader:
-                outfilewriter.writerow(row[2:-1])
+                outfilewriter.writerow(row[1:])
 
             outfile.close()
-
-
